df4198d1f10ca1f797971ff1d527cb56f85b169b
Kamalpreet Singh <kamalpreet192001@gmail.com>
2025-03-30 12:58:51 -0400

    added modelAPI

--- reastraunt/urls.py
+++ reastraunt/urls.py
@@ -1,17 +1,39 @@
-# from django.contrib import admin
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('', views.index),
-#     path('home/',views.home, name ='home'),
-# ]
-
-
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+# router.register(r'tables', views.BookingViewSet)
+
 urlpatterns = [
-    path('', views.index, name='index'),  # Index page
-    path('home/', views.home, name='home'),  # Home page
+    # my views
+    # path('booking-table',views.BookingTableView.as_view()),
+    # path('booking-table/<int:pk>',views.SingleBookingTableView.as_view()),
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleItemView.as_view()),
+
+
+
+
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   # path('index/', views.index, name='index'),  # Index page
+    # path('home/', views.home, name='home'),  # Home page
